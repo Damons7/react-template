ab703606dfee0fc393c71b707523a064ec16afde
ethan.li <ethan.li@idreamsky.com>
2023-02-13 20:53:26 +0800

    feat: init

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,7 +1,6 @@
-// 测试图片显示以及路径别名
-import errorIcon from '@icons/error.png';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
+import './index.less'
 
 export const Mine = () => {
   const navigate = useNavigate();
@@ -9,9 +8,8 @@ export const Mine = () => {
     navigate(-1);
   };
   return (
-    <div className="App">
+    <div className="mine">
       我的页面
-      <img src={errorIcon} alt="" />
       <Button onClick={handleOnClick}>返回上一页</Button>
     </div>
   );
